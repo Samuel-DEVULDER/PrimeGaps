99847a99f8bf0b1c84879c69f64af0a58e60ec68
Samuel DEVULDER <Utilisateur@W520>
2026-03-13 00:36:06 +0100

    forces vector-module

--- src/primegap/sieve/simd/SIMDSieveGap.java
+++ src/primegap/sieve/simd/SIMDSieveGap.java
@@ -2,8 +2,11 @@ package primegap.sieve.simd;
 
 import primegap.sieve.SieveGap;
 import primegap.sieve.SlidingWindowSieve;
+import primegap.util.Machine;
 
 public class SIMDSieveGap extends SieveGap {
+	static public boolean enabled = Machine.enableSIMD();
+	
 	@Override
 	protected SlidingWindowSieve newSlidingWindowSieve(int size) {
 		return new SIMDSlidingWindowSieve(this, size);
