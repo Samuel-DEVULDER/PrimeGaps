8526bc0ed01c7021502c6dbee8f2b64d79a9fcfc
Samuel DEVULDER <samuel.devulder@laposte.net>
2026-04-10 18:30:48 +0200

    Replace NaiveGap with ParallelMillerRabinGap implementation

---
+++ src/primegap/naive/ParallelMillerRabinGap.java
@@ -0,0 +1,19 @@
+package primegap.naive;
+
+import java.math.BigInteger;
+
+import primegap.BigIntegerNextProbablePrimeGap;
+
+public class ParallelMillerRabinGap extends BigIntegerNextProbablePrimeGap {
+	@Override
+	protected BigInteger nextPrimeImpl(BigInteger N) {
+		BigInteger P = N.add(N.testBit(0) ? TWO : ONE);
+		while (!isPrime(P))
+			P = P.add(TWO);
+		return P;
+	}
+
+	public static void main(String[] args) throws Exception {
+		new ParallelMillerRabinGap().run();
+	}
+}
