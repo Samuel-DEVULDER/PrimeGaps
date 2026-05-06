4f706c82b63f4e49e59e653f10ecfa056075ebfa
Samuel DEVULDER <Utilisateur@W520>
2026-05-06 20:13:26 +0200

    Added upTo(limit) which returns a sub-collection in a fast way.

--- src/primegap/util/IncreasingBigIntegers.java
+++ src/primegap/util/IncreasingBigIntegers.java
@@ -4,10 +4,16 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.lang.ref.Cleaner;
+import java.lang.ref.SoftReference;
 import java.math.BigInteger;
 import java.util.AbstractCollection;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * This class implements a collection of increasing BigIntegers that are stored
@@ -182,65 +188,92 @@ public class IncreasingBigIntegers extends AbstractCollection<BigInteger> implem
 
 	@Override
 	public Iterator<BigInteger> iterator() {
-		return new Iterator<BigInteger>() {
-			byte[] buf = new byte[buffer.length];
-			BigInteger next;
-			long pos = 0;
-			int buf_len, buf_idx;
+		return new Itr();
+	}
 
-			{
-				advance();
-			}
+	class Itr implements Iterator<BigInteger> {
+		Itr() {
+			advance();
+		}
 
-			@Override
-			public boolean hasNext() {
-				return next != null;
-			}
+		Itr(State state) {
+			this.next = state.next;
+			this.pos = state.pos & Long.MAX_VALUE;
+			fill_buf();
+			this.buf_idx = state.buf_idx;
+			advance();
+		}
 
-			@Override
-			public BigInteger next() {
-				BigInteger next = this.next;
-				advance();
-				return next;
-			}
+		byte[] buf = new byte[buffer.length];
+		BigInteger next, nonnull_next;
+		long pos = 0;
+		int buf_len, buf_idx;
 
-			boolean fill_buf() {
-				try {
-					if (pos < 0) {
-						return false;
-					}
-					synchronized (raf) {
-						raf.seek(pos);
-						buf_len = raf.read(buf);
-						if (buf_len < 0) {
-							pos = -1;
-							System.arraycopy(buffer, 0, buf, 0, buf_len = index);
-						} else {
-							pos = raf.getFilePointer();
-						}
-					}
-					buf_idx = 0;
-					return buf_len > 0;
-				} catch (IOException ex) {
-					throw new RuntimeException(ex);
+		public static record State(BigInteger next, long pos, int buf_idx) {
+
+		}
+
+		public State getState() {
+			return new State(nonnull_next, pos, buf_idx);
+		}
+
+		@Override
+		public boolean hasNext() {
+			return next != null;
+		}
+
+		@Override
+		public BigInteger next() {
+			BigInteger next = this.next;
+			advance();
+			return next;
+		}
+
+		protected boolean cannot_fill() {
+			return pos < 0;
+		}
+
+		protected void disable_fill() {
+			pos |= Long.MIN_VALUE;
+		}
+
+		boolean fill_buf() {
+			try {
+				if (cannot_fill()) {
+					return false;
 				}
-			}
-
-			void advance() {
-				byte v;
-				for (int k = v = 0; v >= 0; k += 7) {
-					if (buf_idx == buf_len) {
-						if (!fill_buf()) {
-							next = null;
-							return;
-						}
+				synchronized (raf) {
+					raf.seek(pos);
+					buf_len = raf.read(buf);
+					if (buf_len < 0) {
+						disable_fill();
+						System.arraycopy(buffer, 0, buf, 0, buf_len = index);
+					} else {
+						pos = raf.getFilePointer();
 					}
-					v = buf[buf_idx++];
-					BigInteger delta = BigInteger.valueOf(127 & v).shiftLeft(k);
-					next = next == null ? delta : next.add(delta);
 				}
+				buf_idx = 0;
+				return buf_len > 0;
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
 			}
-		};
+		}
+
+		void advance() {
+			byte v;
+			for (int k = v = 0; v >= 0; k += 7) {
+				if (buf_idx == buf_len) {
+					if (!fill_buf()) {
+						nonnull_next = next;
+						next = null;
+						return;
+					}
+				}
+				v = buf[buf_idx++];
+				BigInteger delta = BigInteger.valueOf(127 & v).shiftLeft(k);
+				nonnull_next = next = next == null ? delta : next.add(delta);
+			}
+		}
 	}
 
 	@Override
@@ -253,6 +286,128 @@ public class IncreasingBigIntegers extends AbstractCollection<BigInteger> implem
 
 	public long limit() {
 		return LIMIT;
+	}
+
+	public static BigInteger[] EMPTY = new BigInteger[0];
+	class LimitedSubCollection {
+		SoftReference<BigInteger[]> collected = new SoftReference<BigInteger[]>(EMPTY);
+
+		Itr.State state = null;
+
+		int limit_idx;
+
+		protected int findIndexGT(BigInteger[] array, int from, BigInteger limit) {
+			if (from == array.length || limit.compareTo(array[from]) < 0)
+				return from;
+			++from;
+			if (from == array.length || limit.compareTo(array[from]) < 0)
+				return from;
+
+			int a = from, b = array.length;
+			while (b - a > 1) {
+				int c = a + (b - a) / 2;
+				int d = array[c].compareTo(limit);
+				if (d <= 0)
+					a = c;
+				else
+					b = c;
+			}
+			return b;
+		}
+
+		public Collection<BigInteger> collectUpTo(BigInteger limit_included) {
+			if (collected != null)
+				try {
+					var array = collected.get();
+					if (array == null) {
+						array = EMPTY;
+						state = null;
+						limit_idx = 0;
+					}
+					if (array.length == 0 || limit_included.compareTo(array[array.length - 1]) > 0) {
+						List<BigInteger> compl = new ArrayList<>();
+
+						Itr itr = state == null ? new Itr() : new Itr(state);
+
+						while (itr.hasNext()) {
+							compl.add(itr.next());
+							if (last.compareTo(limit_included) > 0) {
+								itr.disable_fill();
+							}
+						}
+						state = itr.getState();
+
+						int len = array.length;
+						array = Arrays.copyOf(array, len + compl.size());
+						for (BigInteger b : compl)
+							array[len++] = b;
+						compl.clear();
+						collected = new SoftReference<BigInteger[]>(array);
+					}
+					limit_idx = findIndexGT(array, limit_idx, limit_included);
+					var final_array = array;
+					var final_size = limit_idx;
+					var ret = new AbstractList<BigInteger>() {
+						@Override
+						public int size() {
+							return final_size;
+						}
+
+						@SuppressWarnings("unchecked")
+						@Override
+						public <T> T[] toArray(T[] a) {
+							if (a == EMPTY)
+								return (T[]) final_array;
+							return super.toArray(a);
+						}
+
+						@Override
+						public Object[] toArray() {
+							return final_array;
+						}
+
+						@Override
+						public BigInteger get(int index) {
+							return final_array[index];
+						}
+					};
+					return ret;
+				} catch (OutOfMemoryError oem) {
+					collected = null;
+				}
+
+			return new AbstractCollection<BigInteger>() {
+				int size = -1;
+
+				@Override
+				public Iterator<BigInteger> iterator() {
+					return new Itr() {
+						@Override
+						public boolean hasNext() {
+							return super.hasNext() && next.compareTo(limit_included) <= 0;
+						}
+					};
+				}
+
+				@Override
+				public int size() {
+					int n = size;
+					if (n < 0) {
+						n = 0;
+						for (Iterator<BigInteger> i = iterator(); i.hasNext() && n < Integer.MAX_VALUE; i.next())
+							++n;
+						size = n;
+					}
+					return n;
+				}
+			};
+		}
+	}
+
+	LimitedSubCollection subCol = new LimitedSubCollection();
+
+	synchronized public Collection<BigInteger> upTo(BigInteger limit_included) {
+		return isEmpty() ? Collections.emptyList() : subCol.collectUpTo(limit_included);
 	}
 
 }
