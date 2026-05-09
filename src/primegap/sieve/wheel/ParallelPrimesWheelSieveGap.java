4e7920309a05dd562e982143d214456c3fa052ed
Samuel DEVULDER <Utilisateur@W520>
2026-05-09 12:10:08 +0200

    Add ParallelPrimesWheelSieveGap class for parallel prime gap calculations using WHEEL210.

---
+++ src/primegap/sieve/wheel/ParallelPrimesWheelSieveGap.java
@@ -0,0 +1,93 @@
+package primegap.sieve.wheel;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+
+import primegap.sieve.AbstractSlidingWindowSieve;
+import primegap.util.IncreasingBigIntegers;
+import primegap.util.Java;
+
+public class ParallelPrimesWheelSieveGap extends AbstractWheelSieveGap {
+
+	protected AbstractSlidingWindowSieve newSlidingWindowSieve(int size, boolean doubleBuffer) {
+		return new WheelSieve(size, doubleBuffer, 2, 3, 5, 7) {
+			@Override
+			protected String name() {
+				if (name == null)
+					name = super.name() + "/ParallelPrimes";
+				return name;
+			}
+
+			private String name;
+
+			static final VarHandle VH = MethodHandles.arrayElementVarHandle(long[].class);
+
+			@Override
+			protected void updateTab(long[] tab, int i, long mask) {
+				if ((tab[i] & mask) != mask)
+					VH.getAndBitwiseOr(tab, i, mask);
+			}
+
+			@Override
+			protected void doMarkAllMultiples(BigInteger start, long[] tab, IncreasingBigIntegers primes,
+					BigInteger limit) {
+				fillTab(tab, 0);
+				if (primes.isEmpty())
+					return;
+
+				assert Java.dbgTic();
+
+				var col = primes.upTo(limit);
+				var array = col.toArray(IncreasingBigIntegers.EMPTY);
+
+				@SuppressWarnings("unused")
+				var stream = false ? IntStream.range(0, col.size()) : Java.shuffledRange(0, col.size());
+				stream.parallel().forEach(i -> markMultiplesOf(start, tab, array[i]));
+				assert Java.dbg("all(parallel)=", Java.dbgToc(), "ms                  ");
+			}
+		};
+	}
+
+	public static void main(String[] args) {
+		new ParallelPrimesWheelSieveGap().run();
+	}
+
+	static public class DoubleBuffer extends ParallelPrimesWheelSieveGap {
+		@Override
+		protected AbstractSlidingWindowSieve newSlidingWindowSieve(int size) {
+			return newSlidingWindowSieve(size, true);
+		}
+
+		public static void main(String[] args) {
+			new DoubleBuffer().run();
+		}
+	}
+
+	public static class FastForward extends ParallelPrimesWheelSieveGap {
+		public FastForward() {
+			gapCounts = null;
+		}
+
+		@Override
+		protected BigInteger fastForward(BigInteger P, int gap) {
+			return supplier.fastForward(P, gap, this::addPrimeCount);
+		}
+
+		public static void main(String[] args) {
+			new FastForward().run();
+		}
+
+		static public class DoubleBuffer extends FastForward {
+			@Override
+			protected AbstractSlidingWindowSieve newSlidingWindowSieve(int size) {
+				return newSlidingWindowSieve(size, true);
+			}
+
+			public static void main(String[] args) {
+				new DoubleBuffer().run();
+			}
+		}
+	}
+}
