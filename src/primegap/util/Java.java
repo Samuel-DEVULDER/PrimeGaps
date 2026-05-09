703a061a05e9f00f6e27dad314c6b07d0cb48ecd
Samuel DEVULDER <Utilisateur@W520>
2026-05-09 15:58:03 +0200

    reverted ansi esc-sequence for CEOL since it didn't work properly when resizing powershell.

--- src/primegap/util/Java.java
+++ src/primegap/util/Java.java
@@ -382,7 +382,7 @@ public class Java {
 
 	static List<Long> timeStack = new ArrayList<>();
 	public static boolean isTTY = System.console() != null;
-	public static String CEOL = isTTY ? "\\033[K" : new String("");
+	public static String CEOL = new String(isTTY ? "                                                " : "");
 	private static int maxLen = 0;
 
 	public static boolean dbg(Object... args) {
